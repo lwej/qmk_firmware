5667e5eb46cc2b7a8f1d93d923304e8fefb99b47
Leyla Wejdell Bascuñan <leylawbascunan@gmail.com>
2025-03-26 22:42:20 +0100

    bring back the lily58

--- keyboards/lily58/keymaps/lwej/keymap.c
+++ keyboards/lily58/keymaps/lwej/keymap.c
@@ -14,8 +14,7 @@ enum layer_number {
 
 enum td_keycodes {
     _MT_L,
-    T_ESC,
-    T_BSPC
+    T_ESC
 };
 
 typedef enum {
@@ -42,7 +41,7 @@ void mod_tap_layer_reset(tap_dance_state_t *state, void *user_data);
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /* QWERTY
  * ,-----------------------------------------.                    ,-----------------------------------------.
- * | ESC  |   1  |   2  |   3  |   4  |   5  |                    |   6  |   7  |   8  |   9  |   0  |BSPC_~|
+ * | ESC  |   1  |   2  |   3  |   4  |   5  |                    |   6  |   7  |   8  |   9  |   0  | BSPC |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
  * | Tab  |   Q  |   W  |   E  |   R  |   T  |                    |   Y  |   U  |   I  |   O  |   P  |  Å   |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
@@ -56,11 +55,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 
  [_QWERTY] = LAYOUT(
-  KC_ESC,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                     KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    TD(T_BSPC),
-  KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    SE_ARNG,
-  KC_LCTL,  KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                     KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
-  KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, XXXXXXX,   XXXXXXX, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  KC_RSFT,
-                       KC_LALT, KC_LGUI, MO(_LOWER), KC_SPC,    KC_ENT, MO(_RAISE), KC_BSPC, KC_RGUI
+  KC_ESC,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                   KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
+  KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                   KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC,
+  KC_LCTL,  KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                   KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
+  KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, XXXXXXX, XXXXXXX, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ESC,
+                      KC_LALT, TD(_MT_L), MO(_LOWER), KC_SPC, KC_ENT, MO(_RAISE), KC_BSPC, KC_RGUI
 ),
 /* LOWER
  * ,-----------------------------------------.                    ,-----------------------------------------.
@@ -78,10 +77,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
  [_LOWER] = LAYOUT(
   KC_F1,     KC_F2,    KC_F3,    KC_F4,    KC_F5,   KC_F6,                     KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,
-  _______,   SE_EXLM,  SE_DQUO,  SE_HASH,  SE_DLR,  SE_PERC,                   SE_AMPR,  SE_SLSH,  SE_LPRN,  SE_RPRN,  SE_EQL,   KC_DEL,
+  TD(T_ESC), SE_EXLM,  SE_DQUO,  SE_HASH,  SE_DLR,  SE_PERC,                   SE_AMPR,  SE_SLSH,  SE_LPRN,  SE_RPRN,  SE_EQL,   KC_DEL,
   _______,   SE_LPRN,  SE_RPRN,  SE_LCBR,  SE_RCBR, SE_ARNG,                   KC_LEFT,  KC_DOWN,  KC_UP,    KC_RGHT,  KC_GRV,   SE_ASTR,
-  SE_PIPE,   SE_LBRC,  SE_RBRC,  SE_SLSH,  SE_BSLS, SE_ASTR, XXXXXXX, XXXXXXX, KC_C_L,   KC_C_R,   SE_LABK,  SE_RABK,  SE_PLUS,  RSFT_T(KC_SPC),
-                            _______, _______, _______, _______,      _______,   MO(3),   _______, _______
+  _______,   SE_LBRC,  SE_RBRC,  SE_SLSH,  SE_BSLS, SE_ASTR, _______, _______, KC_C_L,   KC_C_R,   SE_LABK,  SE_RABK,  SE_PLUS,  RSFT_T(KC_SPC),
+                                  _______, _______, _______, _______, _______,   MO(3),   _______, _______
 ),
 /* RAISE
 * ,-----------------------------------------.                    ,-----------------------------------------.
@@ -99,12 +98,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 */
 
  [_RAISE] = LAYOUT(
-TD(T_ESC),  SE_ACUT,  SE_AT,   SE_GRV, _______, _______,                      SE_CIRC, _______, _______, _______, _______, SE_SECT,
-  _______,  _______, _______, _______, _______, _______,                      _______, KC_BTN1, KC_BTN2, _______, _______, SE_TILD,
-  _______,  SE_ACUT,  SE_AT,  SE_GRV,  KC_RBRC, SE_QUOT,                      KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R, _______, _______,
-  SE_PIPE,  _______, _______, _______, _______, SE_ASTR, XXXXXXX,    XXXXXXX, KC_WH_L, KC_WH_D, KC_WH_U, KC_WH_R, _______, _______,
-                              _______, _______,   MO(3), _______,    _______,  _______, _______, _______
-),
+  _______, _______, _______, _______, _______, _______,                   _______, _______, _______, _______, _______, _______,
+  SE_TILD,  KC_1,   KC_2,    KC_3,   KC_4,    KC_5,                          KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_DEL,
+  _______,  SE_ACUT,  SE_AT,  SE_GRV,  KC_RBRC, SE_ARNG,                     KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,
+  SE_PIPE,  KC_ESC,  KC_RGUI, KC_RALT, SE_CIRC, SE_QUOT, _______, _______,   KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,
+                              _______, _______,   MO(3), _______, _______,  _______, _______, _______
+  ),
 /* ADJUST
 * ,-----------------------------------------.                    ,-----------------------------------------.
 * | BOOT |      |      |      |      |      |                    |      |      |      |      |      |      |
@@ -119,12 +118,12 @@ TD(T_ESC),  SE_ACUT,  SE_AT,   SE_GRV, _______, _______,                      SE
 *                   |      |      |      |/       /         \      \ |      |      |      |
 *                   `----------------------------'           '------''--------------------'
 */
-[_ADJUST] = LAYOUT(
-QK_BOOT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   XXXXXXX, KC_BTN1, KC_BTN2, XXXXXXX, XXXXXXX, XXXXXXX,
-XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-                            _______, _______, _______, _______, _______,  _______, _______, _______
+ [_ADJUST] = LAYOUT(
+  _______, _______, _______, _______, _______, _______,                    _______, _______, _______, _______, _______, _______,
+  QK_BOOT, _______, _______, _______, _______, _______,                    _______,  KC_BTN1, KC_BTN2, _______, _______, _______,
+  _______, _______, _______, _______, _______, _______,                    KC_MS_L,  KC_MS_D,  KC_MS_U, KC_MS_R, _______,_______,
+  _______,  SE_ACUT,  SE_AT,  SE_GRV,  KC_RBRC, SE_QUOT, _______, _______, KC_WH_L, KC_WH_D, KC_WH_U, KC_WH_R, _______, _______,
+                              _______, _______, _______, _______, _______,  _______, _______, _______
 )
 };
 
@@ -198,7 +197,6 @@ XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
 tap_dance_action_t tap_dance_actions[] = {
     [_MT_L] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, mod_tap_layer_finished, mod_tap_layer_reset),
     [T_ESC] = ACTION_TAP_DANCE_DOUBLE(KC_ESC, SE_TILD),
-    [T_BSPC] = ACTION_TAP_DANCE_DOUBLE(KC_BSPC, SE_TILD)
 };
 
 
